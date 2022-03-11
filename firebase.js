bbf57b12685a3ac1b182f273e7f8d654f6af4b64
Chanakyha <66877639+chanakyha@users.noreply.github.com>
2022-03-12 00:43:06 +0530

    Firebase is initiated

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYJBSzQ3IDKZ9Px9Hv66oe8fA5Ux9ElTU",
+  authDomain: "message-me-844d1.firebaseapp.com",
+  projectId: "message-me-844d1",
+  storageBucket: "message-me-844d1.appspot.com",
+  messagingSenderId: "319333336997",
+  appId: "1:319333336997:web:2906d771a014f650acca4c",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+const provider = new GoogleAuthProvider();
+
+export { app, auth, db, provider };
