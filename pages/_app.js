fbb421deb7af9bcf1334fe2e62d61045cb8e8ff0
Chanakyha <66877639+chanakyha@users.noreply.github.com>
2022-03-12 01:21:52 +0530

    checking login state

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Login from "./login";
+import { auth, db } from "../firebase";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, loading] = useAuthState(auth);
+
+  if (!user) return <Login />;
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
